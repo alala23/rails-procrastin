562634d047b1c13831c5d3b060907b22a5210201
arnaud fabregat <fabregat.arnaud01@gmail.com>
2022-11-29 11:33:23 +0100

    yo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :text, :openai]
 
   def home
+  end
+
+  def text
+  end
+
+  def openai
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "/text", to: "pages#text"
+  get "/openai", to: "pages#openai"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
