2929ea0aabf195d85a147ecbef525292b718331f
arnaud fabregat <fabregat.arnaud01@gmail.com>
2022-11-29 12:52:53 +0100

    yo

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/text", to: "pages#text"
   get "/openai", to: "pages#openai"
-
+  resources :ldms, only: [:new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
